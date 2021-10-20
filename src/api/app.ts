a0b9542580adb8cda1a2150935f97ccd312f8418
Lucas Zocca <lucaszoccasoares@gmail.com>
2021-10-20 15:26:58 -0300

    feat: Add middleware routes and uncouples server

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,7 +1,9 @@
+import { categoriesRoutes } from './routes/categories.routes';
 import express from 'express';
 
 const app = express();
 
 app.use(express.json());
+app.use("/categories", categoriesRoutes);
 
 export { app };
