6e0847a3f11d08a64d458c024e1b76c999990486
Lucas Zocca <lucaszoccasoares@gmail.com>
2021-10-20 16:13:12 -0300

    feat: Add categories validation

--- src/api/routes/categories.routes.ts
+++ src/api/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
+  const categoriesAlreadyExists = categoriesRepository.findByName(name);
+
+  if(categoriesAlreadyExists) {
+    response.status(400).json({error: "Categories already exists"});
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
