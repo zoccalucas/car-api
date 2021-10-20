18e6ab8fb627d0885898d23766933a7e260d12f7
Lucas Zocca <lucaszoccasoares@gmail.com>
2021-10-20 15:59:50 -0300

    feat: Create categories Repository

--- src/api/routes/categories.routes.ts
+++ src/api/routes/categories.routes.ts
@@ -1,25 +1,16 @@
 import { Request, Response, Router } from 'express';
 
-import { Category } from '../../core/model/category';
+import { CategoriesRepository } from '../../core/repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return response.status(201).json(categories);
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
