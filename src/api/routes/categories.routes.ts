b9d1d2a9c0d42e9a58e3c30936dc18ec5fae84ee
Lucas Zocca <lucaszoccasoares@gmail.com>
2021-10-20 15:25:29 -0300

    feat: Add categories Routes and Model

---
+++ src/api/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Request, Response, Router } from 'express';
+
+import { Category } from '../../core/model/category';
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post('/', (request: Request, response: Response) => {
+  const { name, description } = request.body;
+
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
+
+  categories.push(category);
+
+  return response.status(201).json(categories);
+});
+
+export { categoriesRoutes };
