3df0f01466f07802fcf9f1527e2af4ef8e6ef007
Lucas Zocca <lucaszoccasoares@gmail.com>
2021-10-21 11:46:05 -0300

    feat: Add controller and use case index for categories

--- src/api/routes/categories.routes.ts
+++ src/api/routes/categories.routes.ts
@@ -1,22 +1,16 @@
 import { Request, Response, Router } from 'express';
 
 import { CategoriesRepository } from '../../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({name, description});
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
-categoriesRoutes.get("/",  (request: Request, response: Response) => {
+categoriesRoutes.get('/', (request: Request, response: Response) => {
   const all = categoriesRepository.list();
 
   return response.status(201).json(all);
