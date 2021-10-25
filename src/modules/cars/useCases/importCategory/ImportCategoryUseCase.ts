4429ff4e4c90ca71daf97d6b0da8a1fbfe25184d
Lucas Zocca <lucaszoccasoares@gmail.com>
2021-10-25 19:08:32 -0300

    feat: addn upload data on repository

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -15,25 +15,38 @@ class ImportCategoryUseCase {
 
       stream.pipe(parseFile);
 
-      parseFile.on('data', async (line) => {
-        const [name, description] = line;
-        categories.push({
-          name,
-          description,
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (err) => {
+          reject(err);
         });
-      })
-      .on("end", () => {
-        resolve(categories);
-      })
-      .on("error", (err) => {
-        reject(err);
-      });
     });
   }
 
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    console.log(categories);
+
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+    });
   }
 }
 
